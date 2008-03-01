4f741e5257f35efa9f4b6326bfadebb75a00d98f
malte.ubl <malte.ubl@7a55df26-d546-0410-a47d-095215523ac8>
2008-03-01 17:45:40 +0000

    Made Google Gears Support optional. Removed depency on gears_init.js file

--- js/xssinterface.js
+++ js/xssinterface.js
@@ -39,15 +39,16 @@
  * 
  */
 
+var XSSInterfaceEnableGoogleGearsSupport  = true;
 
 // Number of milli seconds between polls for new callbacks
-var XSSInterfacePollIntervalMilliSeconds = 300;
+var XSSInterfacePollIntervalMilliSeconds  = 300;
 // Name of cookie that is used for messages. Should not be changed
-var XSSInterfaceCookieName               = "XSSData";
+var XSSInterfaceCookieName                = "XSSData";
 // Name of cookie that is used for the security token
-var XSSInterfaceSecurityTokenCookieName  = "XSSSecurityToken";
+var XSSInterfaceSecurityTokenCookieName   = "XSSSecurityToken";
 // Enables debug mode. You might want to add <div id=log></div> to your pages
-var XSSInterfaceDebug                    = false;
+var XSSInterfaceDebug                     = false;
 
 
 XSSInterface = {
@@ -59,6 +60,10 @@ XSSInterface = {
 	},
 	
 	canGearsMessage: function () {
+		if(!XSSInterfaceEnableGoogleGearsSupport) {
+			return false
+		}
+		XSSInterfaceInitializeGears(); // Initialize gears. Now that we know that we need it
 		if(google && google.gears && google.gears.factory) {
 			return true
 		}
@@ -144,13 +149,14 @@ XSSInterface.Listener.prototype = {
 				var domain   = this.allowedDomains[i];
 				var url      = "http://"+domain+"/xssinterface/js/gears_listener.js"; // XXX make path variable
 				var workerId = workerPool.createWorkerFromUrl(url);
-				// XXX turn this into a gears worker. At least we know that it is installed at this point
+				// XXX turn this into a gears worker. At least we know that it is installed at this point. 
+				// Having the timer inside the gear seems to cause databse locking problems, though
 				// send message to all workers and check for new message every n milli seconds
-				window.setInterval(function () { workerPool.sendMessage(""+me.channelId, workerId) }, 1000 )
+				window.setInterval(function () { workerPool.sendMessage(""+me.channelId, workerId) }, XSSInterfacePollIntervalMilliSeconds )
 			}
 		}
 		else {
-			winow.setInterval( function () { me.execute(me.parse(), false) }, XSSInterfacePollIntervalMilliSeconds )
+			window.setInterval( function () { me.execute(me.parse(), false) }, XSSInterfacePollIntervalMilliSeconds )
 		}
 	},	
 	
@@ -619,3 +625,93 @@ XSSdebug = function (txt) {
 		alert(message)
 	}
 }
+
+
+
+// Copyright 2007, Google Inc.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//  3. Neither the name of Google Inc. nor the names of its contributors may be
+//     used to endorse or promote products derived from this software without
+//     specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+// EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Sets up google.gears.*, which is *the only* supported way to access Gears.
+//
+// Circumvent this file at your own risk!
+//
+// In the future, Gears may automatically define google.gears.* without this
+// file. Gears may use these objects to transparently fix bugs and compatibility
+// issues. Applications that use the code below will continue to work seamlessly
+// when that happens.
+
+// Sorry Google for modifying this :) 
+function XSSInterfaceInitializeGears() {
+  // We are already defined. Hooray!
+  if (window.google && google.gears) {
+    return;
+  }
+
+  var factory = null;
+
+  // Firefox
+  if (typeof GearsFactory != 'undefined') {
+    factory = new GearsFactory();
+  } else {
+    // IE
+    try {
+      factory = new ActiveXObject('Gears.Factory');
+      // privateSetGlobalObject is only required and supported on WinCE.
+      if (factory.getBuildInfo().indexOf('ie_mobile') != -1) {
+        factory.privateSetGlobalObject(this);
+      }
+    } catch (e) {
+      // Safari
+      if (navigator.mimeTypes["application/x-googlegears"]) {
+        factory = document.createElement("object");
+        factory.style.display = "none";
+        factory.width = 0;
+        factory.height = 0;
+        factory.type = "application/x-googlegears";
+        document.documentElement.appendChild(factory);
+      }
+    }
+  }
+
+  // *Do not* define any objects if Gears is not installed. This mimics the
+  // behavior of Gears defining the objects in the future.
+  if (!factory) {
+    return;
+  }
+
+  // Now set up the objects, being careful not to overwrite anything.
+  //
+  // Note: In Internet Explorer for Windows Mobile, you can't add properties to
+  // the window object. However, global objects are automatically added as
+  // properties of the window object in all browsers.
+  if (!window.google) {
+    google = {};
+  }
+
+  if (!google.gears) {
+    google.gears = {factory: factory};
+  }
+}
+
