7f5b7cf5771f5149c86858769c07b04a1d330895
malte.ubl <malte.ubl@7a55df26-d546-0410-a47d-095215523ac8>
2008-03-19 20:50:36 +0000

    Implemented joosify to turn regular classes into Joose.Classes Implemented new more JavaScript like builder syntax

--- projects/Joose/tests/test.js
+++ projects/Joose/tests/test.js
@@ -251,5 +251,71 @@ ok(!a.isEqual(c), "Equality works for unequal things")
 ok(a.notEqual(c), "Role composition works and notEqual works")
 ok(!a.notEqual(b), "Role composition works and notEqual works for equal things")
 
+diag("joosify");
+
+function RegularClass() {
+	this.a = 1;
+	this.b = 2;
+}
+
+RegularClass.prototype = {
+	para: "test",
+	test: function () { return "world" }
+}
+
+
+joosify("RegularClass", RegularClass);
+
+ok(RegularClass, "RegularClass is still there")
+ok(RegularClass.meta, "RegularClasses now have a meta object")
+var a = new RegularClass();
+ok(a, "We can make a joosified class");
+
+ok(a.meta, "RegularClass objects now have a meta class")
+ok(a.para == "test", "Attributes are still there")
+ok(a.test, "Test function is there");
+ok(a.test() == "world", "Test Function is callable")
+ok(a.meta.can("test"), "The meta class is aware of the test function")
+ok(a.meta.isa(RegularClass), "A RegularClass isa RegularClass")
+
+diag("Methods")
+
+ok(a.test.meta.meta.isa(Joose.Method), "Methods of Joose.Classes are Joose.Methods")
+
+diag("Alternative Building")
+
+Class("Currency2", {
+	does: Eq,
+	has:  ["value", {is: rw}],
+	methods: {
+		initialize: function (value) {
+			this.setValue(value)
+		},
+	
+		isEqual: function (cur) {
+			return this.getValue() == cur.getValue()
+		}
+	}
+})
+
+var a = new Currency2(1);
+var b = new Currency2(1);
+var c = new Currency2(2);
+
+ok(a.isEqual(b), "Equality works")
+ok(b.isEqual(a), "Equality works in both directions")
+ok(!a.isEqual(c), "Equality works for unequal things")
+
+ok(a.notEqual(c), "Role composition works and notEqual works")
+ok(!a.notEqual(b), "Role composition works and notEqual works for equal things")
+try {
+	Class("Invalid", {
+		has: ["test"],
+		invalidBuilder: {}
+	})
+} catch(e) {
+	ok(e.indexOf("Called invalid builder") != -1, "Calling invalid builder throws correct exception")
+}
+
 
 }
