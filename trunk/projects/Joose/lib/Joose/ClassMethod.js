74daf4a32fbf3ebbdbb77d8525e4d46576757a51
malte.ubl <malte.ubl@7a55df26-d546-0410-a47d-095215523ac8>
2008-11-07 21:12:28 +0000

    Project has moved

--- trunk/projects/Joose/lib/Joose/ClassMethod.js
+++
@@ -1,9 +0,0 @@
-Class("Joose.ClassMethod", {
-	isa: Joose.Method,
-	methods: {
-		isClassMethod: function () { return true },
-		addToClass: function (c) {
-			c[this.getName()] = this.asFunction()
-		}
-	}
-})
